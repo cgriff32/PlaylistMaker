197ef9766daf68f8e6624e9cb1c7f2c56088a390
Chris Griffiths <cgriff32@gmail.com>
2018-05-31 15:39:52 +0100

    Added ability to update existing playlists

--- scripts/playlist.py
+++ scripts/playlist.py
@@ -4,6 +4,8 @@ import sys
 import spotipy
 import spotipy.util as util
 
+import secrets
+
 
 def show_tracks(tracks):
 
@@ -15,19 +17,18 @@ def show_tracks(tracks):
 def add_tracks(artistid):
     response = sp.artist_top_tracks(artistid)
     tracklist = list()
-
+    
     for newtrack in response['tracks']:
         tracklist.append(newtrack['uri'])
-
-    newresults = sp.user_playlist_add_tracks(username, newplaylist['id'],
-    tracklist)
+        
+    newresults = sp.user_playlist_add_tracks(username, newplaylist['id'],tracklist)
 
 if __name__ == '__main__':
 
     if len(sys.argv) > 2:
         newplaylistname = sys.argv[2]
     else:
-        newplaylistname = 'New Playlist'
+        newplaylistname = 'Top Tracks'
 
     if len(sys.argv) < 2:
         print (sys.argv[0], 'Username(Required), Playlist Name(Optional)')
@@ -36,15 +37,19 @@ if __name__ == '__main__':
         username = sys.argv[1]
 
     scope = 'playlist-modify-public'
-    token = util.prompt_for_user_token(username,scope)
+    token = util.prompt_for_user_token(username,scope,secrets.CLIENT_ID,secrets.CLIENT_SECRET,secrets.REDIRECT_URI)
 
     if token:
         sp = spotipy.Spotify(auth=token)
         playlists = sp.user_playlists(username)
-        newplaylist = sp.user_playlist_create(username, newplaylistname)
+        temp = 0
 
         for playlist in playlists['items']:
             if playlist['owner']['id'] == username:
+                if playlist['name'] == newplaylistname:
+                    print("%s    %s" % (newplaylistname, playlist['name']))
+                    newplaylist = sp.user_playlist(username, playlist['id'])
+                    temp = 1
 
                 if playlist['name'] == 'Artists':
                     print (playlist['name'])
@@ -52,9 +57,17 @@ if __name__ == '__main__':
                     results = sp.user_playlist(username, playlist['id'],
                         fields="tracks,next")
                     tracks = results['tracks']
-                    show_tracks(tracks)
-                    while tracks['next']:
-                        tracks = sp.next(tracks)
-                        show_tracks(tracks)
+
+                        
+        if temp == 0:
+            newplaylist = sp.user_playlist_create(username, newplaylistname) 
+            
+        newresults = sp.user_playlist_replace_tracks(username, newplaylist['id'],list()) 
+        show_tracks(tracks)
+        
+        while tracks['next']:
+            tracks = sp.next(tracks)
+            show_tracks(tracks)
+
     else:
         print ("Can't get token for", username)
