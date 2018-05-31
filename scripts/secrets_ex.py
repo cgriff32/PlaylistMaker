f78f018a7e0f116a6deadbc76b36019050ab7388
Chris Griffiths <cgriff32@gmail.com>
2018-05-31 15:43:13 +0100

    Added secrets.py to store client id

---
+++ scripts/secrets_ex.py
@@ -0,0 +1,6 @@
+#Rename this file "secrets.py" before using
+
+#from spotify developers website
+CLIENT_ID = '' #Your client ID here
+CLIENT_SECRET = '' #Your client secret here
+REDIRECT_URI = 'http://localhost/'
